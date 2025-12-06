e71600c6bc89fb5cc06575df1e3aa738d5e367ce
HTMonkeyG <3254339747@qq.com>
2025-12-07 02:24:41 +0800

    Add duplicate test.

--- dll/src/dllmain.c
+++ dll/src/dllmain.c
@@ -4,7 +4,9 @@
 #include "svr.h"
 
 HMODULE hDllNcmAudioPlayer = NULL;
-i08 gActive = 0;
+
+static HANDLE hMutex = NULL;
+static i08 gActive = 0;
 
 static DWORD WINAPI svrWaitForNcm(
   LPVOID lpParam
@@ -31,6 +33,16 @@ BOOL APIENTRY DllMain(
     if (!GetModuleHandleA("Sky.exe"))
       return TRUE;
 
+    // In most cases, there will only be one game instance. This code is used
+    // to prevent duplicate injection.
+    // If want to disable it, comment the #define statement in svr.h
+#ifdef svrDuplicateCheck
+    SetLastError(ERROR_SUCCESS);
+    hMutex = CreateMutexW(NULL, FALSE, L"__SKY_VOLRST__");
+    if (GetLastError() == ERROR_ALREADY_EXISTS)
+      return TRUE;
+#endif
+
     gActive = 1;
     MH_Initialize();
     CreateThread(
@@ -40,9 +52,13 @@ BOOL APIENTRY DllMain(
       NULL,
       0,
       NULL);
-  } else if (dwReason == DLL_PROCESS_DETACH && gActive) {
-    MH_DisableHook(MH_ALL_HOOKS);
-    MH_Uninitialize();
+  } else if (dwReason == DLL_PROCESS_DETACH) {
+    if (gActive) {
+      MH_DisableHook(MH_ALL_HOOKS);
+      MH_Uninitialize();
+    }
+    if (hMutex)
+      CloseHandle(hMutex);
   }
 
   return TRUE;
